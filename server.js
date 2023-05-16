f3191670e54ff632e2a2d99bc7c3e8c19c755138
victormsti <victormsti@gmail.com>
2023-05-16 17:39:27 -0300

    code refactoring on server.js

--- server.js
+++ server.js
@@ -10,20 +10,24 @@ app.use(cors());
 
 // Initializing DB
 mongoose.connect('mongodb://localhost:27017/light-my-mind',
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
-    }
-);
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+    console.log('successfully connected to the database');
+}).catch(err => {
+    console.log('error connecting to the database');
+    process.exit();
+});
 
 requireDir('./src/models');
 
 // Routes
-// Recieves every kind of request
+// Receives every kind of request
 app.use('/api', require('./src/routes.js'));
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 // Setting port
 //app.listen(process.env.PORT || '0.0.0.0');
 app.listen(port, host, function() {
